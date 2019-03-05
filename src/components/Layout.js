50bd663ce3a0e25c9a598258690fb3be97771b57
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-03-04 21:29:52 -0700

    Add theming
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,39 +1,47 @@
 import React from 'react';
+import { Global, css } from '@emotion/core';
+import { ThemeProvider } from 'emotion-theming';
+
 import Header from './Header';
 import Footer from './Footer';
 
-import { Global, css } from '@emotion/core';
-
-export default props => (
-  <div
-    css={css`
-      display: flex;
-      flex-direction: column;
-      min-height: 100vh;
-    `}
-  >
-    <Global
-      styles={css`
-        * {
-          box-sizing: border-box;
-        }
-        body {
-          margin: 0;
-        }
-        a {
-          color: white;
-        }
-      `}
-    />
-    <Header />
-    <div
-      css={css`
-        flex: 1;
-        padding: 1em;
-      `}
-    >
-      {props.children}
-    </div>
-    <Footer />
-  </div>
-);
+export default props => {
+  const theme = props.theme || {};
+  return (
+    <ThemeProvider theme={theme}>
+      <div
+        css={theme => css`
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+          background-color: ${theme.backgroundColor};
+          color: ${theme.color};
+        `}
+      >
+        <Global
+          styles={css`
+            * {
+              box-sizing: border-box;
+            }
+            body {
+              margin: 0;
+            }
+            a {
+              color: white;
+            }
+          `}
+        />
+        <Header />
+        <div
+          css={css`
+            flex: 1;
+            padding: 1em;
+          `}
+        >
+          {props.children}
+        </div>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
+};
--- src/pages/cleft-templar.js
+++ src/pages/cleft-templar.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Container from '../components/Container';
 
+const theme = {
+  backgroundColor: 'black',
+  color: 'white',
+};
+
 export default () => (
   <div>
-    <Layout>
+    <Layout theme={theme}>
       <Container size={1000}>
         <h1>Cleft Templar</h1>
         <p>A game.</p>
