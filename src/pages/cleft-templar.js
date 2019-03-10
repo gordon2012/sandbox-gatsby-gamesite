e4e8220595a0368eb23357344684cd882faae883
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-03-09 23:47:27 -0700

    Add expansion section
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/pages/cleft-templar.js
+++ src/pages/cleft-templar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Box, Image } from '@rebass/emotion';
+import { Flex, Box, Image, Heading, Text } from '@rebass/emotion';
 
 import Layout from '../components/Layout';
 import Container from '../components/Container';
@@ -14,7 +14,7 @@ const theme = {
 export default () => (
   <div>
     <Layout theme={theme}>
-      <Container s={1000}>
+      <Container s={1200}>
         <Hero img="https://source.unsplash.com/LlXzILHulTM/800x1000" h={600}>
           <Container p="0 1em">
             <h1>Cleft Templar</h1>
@@ -27,7 +27,7 @@ export default () => (
         <Flex>
           <Box width={1 / 2} p={2}>
             <Image
-              src="https://source.unsplash.com/eSNjFDbw_i4/640x480"
+              src="https://source.unsplash.com/GfJfHC-3yWc/800x450"
               alt="Reticulated Splines"
             />
           </Box>
@@ -59,7 +59,7 @@ export default () => (
           </Box>
           <Box width={1 / 2} p={2}>
             <Image
-              src="https://source.unsplash.com/weekly?water/640x480"
+              src="https://source.unsplash.com/IWKjV_nhhOY/800x450"
               alt="Amazing Vigilance"
             />
           </Box>
@@ -68,7 +68,7 @@ export default () => (
         <Flex>
           <Box width={1 / 2} p={2}>
             <Image
-              src="https://source.unsplash.com/weekly?fire/640x480"
+              src="https://source.unsplash.com/MYjFOiVWWT8/800x450"
               alt="Splendid! Such Wow"
             />
           </Box>
@@ -88,6 +88,9 @@ export default () => (
           <Video src="hA6hldpSTF8" ratio={53.4} />
         </Box>
 
+        <Heading className="text-center" mb={3}>
+          Purchase Cleft Templar Now!
+        </Heading>
         <Box px={[5, 0]}>
           {[
             'urioWbjYDOU',
@@ -106,6 +109,56 @@ export default () => (
             />
           ))}
         </Box>
+
+        <Heading
+          className="text-center"
+          fontWeight="normal"
+          fontSize={6}
+          mt={5}
+          mb={4}
+        >
+          Cleft Templar Redefines Modularity with 4 Expansion Packs
+        </Heading>
+        <Flex flexWrap="wrap">
+          {[
+            {
+              img: 'urioWbjYDOU',
+              heading: 'Legendary Warrior',
+              copy:
+                'Battle hordes of foes and smite them with your mighty, powerful weapons.',
+            },
+            {
+              img: 'vIAim1sjf9g',
+              heading: 'Stalwart Defender',
+              copy: 'Protect the innocent with your powerful shield.',
+            },
+            {
+              img: 'OXv_lF1PFiY',
+              heading: 'Rampaging Berserker',
+              copy:
+                'Wildly annihilate anyone foolish enough to get in your way.',
+            },
+            {
+              img: '2QolB3aMw14',
+              heading: 'Cunning Assassin',
+              copy:
+                'Decimate your foes with well placed blows to vital organs.',
+            },
+          ].map((e, i) => (
+            <Box key={e.img} width={[1 / 2, 1 / 4]} px={[4, 3]} py={3}>
+              <Heading
+                className="text-center"
+                fontWeight="normal"
+                fontSize={[3, 1, 2, 4]}
+                mb={2}
+              >
+                {e.heading}
+              </Heading>
+              <Image src={`https://source.unsplash.com/${e.img}/500x500`} />
+              <Text fontSize={[1, 2]}>{e.copy}</Text>
+            </Box>
+          ))}
+        </Flex>
       </Container>
     </Layout>
   </div>
